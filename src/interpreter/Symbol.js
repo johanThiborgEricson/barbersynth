6f3db3d9f14ed55a52f2a4d629e31187df31d1fa
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-10-29 11:31:53 +0100

    Removed unused method.

--- src/interpreter/Symbol.js
+++ src/interpreter/Symbol.js
@@ -23,11 +23,6 @@ Symbol.prototype
 
 Symbol.PARSE_ERROR = new Error("Symbol.PARSE_ERROR");
 
-Symbol.prototype
-.regExpParseInstaller = MethodInstaller(RegExp.prototype, "parse", function(unparsedCodePointer) {
-  return Terminal(this, Symbol().id).parse(unparsedCodePointer);
-});
-
 Symbol
 .regExpParseInstaller = function(Terminal) {
   return MethodInstaller(RegExp.prototype, "parse", function(unparsedCodePointer) {
