1bb6b43f22b2ca607227333516eb33b639109521
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-10-30 06:57:31 +0100

    Terminal.lexemeHead functionality moved to CodePointer.parse.

--- src/interpreter/CodePointer.js
+++ src/interpreter/CodePointer.js
@@ -1,8 +1,14 @@
 function CodePointer(code) {
   var that = Object.create(CodePointer.prototype);
   that.value = code;
-  that.parse = function() {
+  that.parse = function(token) {
+    var match = token.exec(this.value);
+    if(!match || match.index !== 0) {
+      return null;
+    }
     
+    this.value = this.value.slice(match[0].length);
+    return match.slice(1);
   };
   
   that.getUnparsed = function() {
--- src/interpreter/Terminal.js
+++ src/interpreter/Terminal.js
@@ -14,19 +14,6 @@ function Terminal(token, interpretation){
     return instruction;
   };
   
-  // TODO: this function should take the token and be in a Parser object
-  // that is passed to Symbol().parse instead.
-  // FIXME: then the Symbol().parse method should be renamed. To what?
-  that.lexemeHead = function(unparsedCodePointer) {
-    var match = token.exec(unparsedCodePointer.value);
-    if(!match || match.index !== 0) {
-      return null;
-    }
-    
-    unparsedCodePointer.value = unparsedCodePointer.value.slice(match[0].length);
-    return match.slice(1);
-  };
-  
   return that;
 }
 
