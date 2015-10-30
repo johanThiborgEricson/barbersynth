16d8c27c3acf8d21d58c19f8c2dd37fdda926461
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-10-30 09:49:10 +0100

    CodePointer new methods (backup|restore) and some optimization.

--- src/interpreter/CodePointer.js
+++ src/interpreter/CodePointer.js
@@ -1,17 +1,28 @@
 function CodePointer(code) {
   var that = Object.create(CodePointer.prototype);
+  var pointer = 0;
+  
   that.parse = function(token) {
-    var match = token.exec(code);
+    var unparsedCode = code.slice(pointer);
+    var match = token.exec(unparsedCode);
     if(!match || match.index !== 0) {
       return null;
     }
     
-    code = code.slice(match[0].length);
+    pointer += match[0].length;
     return match.slice(1);
   };
   
+  that.backup = function() {
+    return pointer;
+  };
+  
+  that.restore = function(backup) {
+    pointer = backup;
+  };
+  
   that.getUnparsed = function() {
-    return code;
+    return code.slice(pointer);
   };
   
   return that;
