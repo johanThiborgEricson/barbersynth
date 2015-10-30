d411f50cd95a18d8c0bd02a6de34d96263a37b85
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-10-30 08:33:51 +0100

    Removed deprecated parse method.

--- src/interpreter/NonTerminalAlternative.js
+++ src/interpreter/NonTerminalAlternative.js
@@ -1,16 +1,6 @@
 function NonTerminalAlternative(alternatives) {
   var that = Object.create(NonTerminalAlternative.prototype);
   
-  that.parse = function(unparsedCodePointer) {
-    var unparsedCodePointerBackup = unparsedCodePointer.value;
-    var instruction = alternatives.reduce(function(instruction, alternative) {
-      unparsedCodePointer.value = unparsedCodePointerBackup;
-      return instruction || alternative.parse(unparsedCodePointer);
-    }, null);
-    
-    return instruction;
-  };
-  
   that.makeInstruction = function(unparsedCodePointer) {
     var unparsedCodePointerBackup = unparsedCodePointer.value;
     var instruction = alternatives.reduce(function(instruction, alternative) {
