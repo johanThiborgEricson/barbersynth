173519bc088b1bcb567f377efb4b029685aa8515
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-10-30 08:34:56 +0100

    Removed deprecated parse-method.

--- src/interpreter/NonTerminalSequence.js
+++ src/interpreter/NonTerminalSequence.js
@@ -1,30 +1,6 @@
 function NonTerminalSequence(symbols) {
   var that = Object.create(NonTerminalSequence.prototype);
-  that.parse = function(unparsedCodePointer) {
-    var instructions = [];
-    var lastMade = true;
-    
-    var makeInstructionIfAllPreviousWasSuccessful = function(symbol) {
-      lastMade = lastMade && symbol.parse(unparsedCodePointer);
-      instructions.push(lastMade);
-    };
-    
-    symbols.map(makeInstructionIfAllPreviousWasSuccessful);
-    
-    if(!lastMade) {
-      return null;
-    }
-    
-    var instruction = function() {
-      var that = this;
-      instructions.map(function(instruction) {
-        instruction.call(that);
-      });
-    };
-    
-    return instruction;
-  };
-  
+
   that.makeInstruction = function(unparsedCodePointer) {
     var instructions = [];
     var lastMade = true;
