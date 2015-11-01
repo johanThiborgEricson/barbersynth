da0f6ce8cce6e2bbccc56d7346390c82f3d4e6e0
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-11-01 19:16:05 +0100

    Moved the semantic symbols to the prototype.

--- src/LilyPondInterpreter.js
+++ src/LilyPondInterpreter.js
@@ -1,86 +1,121 @@
 function LilyPondInterpreter() {
   var that = Object.create(LilyPondInterpreter.prototype);
 
-  that.absoluteNatural = that.terminal(/([a-g])/, function(naturalName) {
+  return that;
+}
+
+(function() {
+  var semanticSymbol = Interpreter();
+  
+  LilyPondInterpreter.prototype.absoluteNatural = semanticSymbol
+  .terminal(/([a-g])/, function(naturalName) {
     var minusA = naturalName.charCodeAt(0) - "a".charCodeAt(0);
     var lilyPondIndex = (minusA + 5) % 7;
     this.natural = lilyPondIndex - 12;
     this.tone = this.natural2tone(this.natural);
   });
   
-  that.octavationDown = that.terminal(/(,+)/, function(commaString) {
+  LilyPondInterpreter.prototype.octavationDown = semanticSymbol
+  .terminal(/(,+)/, function(commaString) {
     var commaCount = commaString.length;
     this.natural -= 7 * commaCount;
     this.tone -= 12 * commaCount;
   });
   
-  that.octavationUp = that.terminal(/('+)/, function(apostrofeString) {
+  LilyPondInterpreter.prototype.octavationUp = semanticSymbol
+  .terminal(/('+)/, function(apostrofeString) {
     var apostrofeCount = apostrofeString.length;
     this.natural += 7 * apostrofeCount;
     this.tone += 12 * apostrofeCount;
   });
   
   // TODO: move this to Interpreter
-  that.nothing = that.terminal(/()/, function() {});
+  LilyPondInterpreter.prototype.nothing = semanticSymbol
+  .terminal(/()/, function() {});
   
-  that.octavation = that.nonTerminalAlternative(
-      [that.octavationDown, that.octavationUp, that.nothing]
-    );
+  LilyPondInterpreter.prototype.octavation = semanticSymbol
+  .nonTerminalAlternative([
+      LilyPondInterpreter.prototype.octavationDown, 
+      LilyPondInterpreter.prototype.octavationUp, 
+      LilyPondInterpreter.prototype.nothing
+    ]);
   
-  that.natural2tone = function(natural) {
+  // TODO: move this to Interpreter
+  LilyPondInterpreter.prototype.nothing = semanticSymbol
+  .terminal(/()/, function() {});
+  
+  LilyPondInterpreter.prototype.octavation = semanticSymbol
+  .nonTerminalAlternative([
+      LilyPondInterpreter.prototype.octavationDown, 
+      LilyPondInterpreter.prototype.octavationUp, 
+      LilyPondInterpreter.prototype.nothing
+    ]);
+  
+  LilyPondInterpreter.prototype.natural2tone = function(natural) {
     var aMinorScaleTone = ((natural % 7) + 7) % 7;
     var octave = (natural - aMinorScaleTone) / 7;
     var aMinorTones = [0, 2, 3, 5, 7, 8, 10];
     return octave * 12 + aMinorTones[aMinorScaleTone];
   };
   
-  that.flat = that.terminal(/((?:es)+)/, function(esesString) {
+  LilyPondInterpreter.prototype.flat = semanticSymbol
+  .terminal(/((?:es)+)/, function(esesString) {
     var esesCount = esesString.length / 2;
     this.tone -= esesCount;
   });
   
-  that.sharp = that.terminal(/((?:is)+)/, function(isesString) {
+  LilyPondInterpreter.prototype.sharp = semanticSymbol
+  .terminal(/((?:is)+)/, function(isesString) {
     var isesCount = isesString.length / 2;
     this.tone += isesCount;
   });
   
-  that.accidentals = that.nonTerminalAlternative(
-      [that.flat, that.sharp, that.nothing]
-    );
+  LilyPondInterpreter.prototype.accidentals = semanticSymbol
+  .nonTerminalAlternative([
+      LilyPondInterpreter.prototype.flat, 
+      LilyPondInterpreter.prototype.sharp, 
+      LilyPondInterpreter.prototype.nothing
+    ]);
   
-  that.reciprocalLength = that.terminal(/(128|64|32|16|8|4|2|1)/, 
-  function(lengthString){
+  LilyPondInterpreter.prototype.reciprocalLength = semanticSymbol
+  .terminal(/(128|64|32|16|8|4|2|1)/, function(lengthString){
     var numerator = 1;
     var denominator = Number(lengthString);
     this.lengthFraction = [numerator, denominator];
   });
   
-  that.unspecifiedLength = that.terminal(/()/, function() {
+  LilyPondInterpreter.prototype.unspecifiedLength = semanticSymbol
+  .terminal(/()/, function() {
     if(!this.lengthFraction) {
       this.lengthFraction = [1, 4];
     }
   });
   
-  that.dots = that.terminal(/(\.+)/, function(dotsString) {
+  LilyPondInterpreter.prototype.dots = semanticSymbol
+  .terminal(/(\.+)/, function(dotsString) {
     var dotCount = dotsString.length;
     this.lengthFraction[0] *= 2 * Math.pow(2, dotCount) - 1;
     this.lengthFraction[1] *= Math.pow(2, dotCount);
   });
   
   // TODO: write nonTerminalQuestionMark as a shorthand for this.
-  that.possiblyDots = that.nonTerminalAlternative(
-      [that.dots, that.nothing]
-    );
+  LilyPondInterpreter.prototype.possiblyDots = semanticSymbol
+  .nonTerminalAlternative([
+      LilyPondInterpreter.prototype.dots, 
+      LilyPondInterpreter.prototype.nothing
+    ]);
   
-  that.possiblyDottedLength = that.nonTerminalSequence(
-      [that.reciprocalLength, that.possiblyDots]
-    );
+  LilyPondInterpreter.prototype.possiblyDottedLength = semanticSymbol
+  .nonTerminalSequence([
+      LilyPondInterpreter.prototype.reciprocalLength, 
+      LilyPondInterpreter.prototype.possiblyDots
+    ]);
   
-  that.noteLength = that.nonTerminalAlternative(
-      [that.possiblyDottedLength, that.unspecifiedLength]
-    );
+  LilyPondInterpreter.prototype.noteLength = semanticSymbol.nonTerminalAlternative([
+      LilyPondInterpreter.prototype.possiblyDottedLength, 
+      LilyPondInterpreter.prototype.unspecifiedLength
+    ]);
 
-  return that;
-}
+})();
+  
 
-LilyPondInterpreter.prototype = Interpreter();
