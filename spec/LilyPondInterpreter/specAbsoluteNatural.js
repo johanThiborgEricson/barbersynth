eb000b96aeb3c8d2be751d5223bc2b8b0829958a
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-11-08 06:50:19 +0100

    LilyPond absolute pitch clarification.

--- spec/LilyPondInterpreter/specAbsoluteNatural.js
+++ spec/LilyPondInterpreter/specAbsoluteNatural.js
@@ -7,7 +7,10 @@ describe("absoluteNatural(naturalName)", function() {
   });
   
   it("returns {natural} as the natural with naturalName in the " + 
-  "LilyPond absolute c-scale", function() {
+  "LilyPond absolute c-scale i. e. the C major scale that starts at C two " +
+  "octavs below concert pitch A", function() {
+    //      a    b    c    d    e   f   g   a   b   c   d   e   f   g   a_440Hz
+    //    -14  -13  -12  -11  -10  -9  -8  -7  -6  -5  -4  -3  -2  -1   0
     var naturalNames = ["c", "d", "e", "f", "g", "a", "b"];
     var expecteds = [-12, -11, -10, -9, -8, -7, -6];
     var actualls = naturalNames.map(function(naturalName) {
