2314a6a523442a20ba27388befde2b3628f582d8
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-11-08 06:42:56 +0100

    CodePointer optimization: made closure private variables pseudo-private.

--- src/interpreter/CodePointer.js
+++ src/interpreter/CodePointer.js
@@ -1,34 +1,39 @@
 function CodePointer(code) {
   var that = Object.create(CodePointer.prototype);
-  var pointer = 0;
-  
-  that.parse = function(token) {
-    var unparsedCode = code.slice(pointer);
-    var match = token.exec(unparsedCode);
-    if(!match || match.index !== 0) {
-      this.reportParseError(token);
-      return null;
-    }
-    
-    pointer += match[0].length;
-    return match.slice(1);
-  };
-  
-  that.backup = function() {
-    return pointer;
-  };
-  
-  that.restore = function(backup) {
-    pointer = backup;
-  };
-  
-  that.getUnparsed = function() {
-    return code.slice(pointer);
-  };
-  
+  that._code = code;
+  that._pointer = 0;
+
   return that;
 }
 
+CodePointer.prototype
+.parse = function(token) {
+  var unparsedCode = this._code.slice(this._pointer);
+  var match = token.exec(unparsedCode);
+  if(!match || match.index !== 0) {
+    this.reportParseError(token);
+    return null;
+  }
+  
+  this._pointer += match[0].length;
+  return match.slice(1);
+};
+
+CodePointer.prototype
+.backup = function() {
+  return this._pointer;
+};
+
+CodePointer.prototype
+.restore = function(backup) {
+  this._pointer = backup;
+};
+
+CodePointer.prototype
+.getUnparsed = function() {
+  return this._code.slice(this._pointer);
+};
+  
 CodePointer.prototype
 .parseErrorDescription = {
   actuallCode: {
