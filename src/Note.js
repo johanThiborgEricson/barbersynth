eb236584ff192b4032b2ad9a07bb8c88a09dcecb
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2015-11-17 14:53:13 +0100

    Note finds nearest partial to a given f_0, if that partial is closer than a half semi-tone.

--- src/Note.js
+++ src/Note.js
@@ -2,7 +2,7 @@ function Note(tone) {
   var that = Object.create(Note.prototype);
   
   that._tone = tone;
-  
+
   return that;
 }
 
@@ -19,8 +19,17 @@ Note.prototype
 };
 
 Note.prototype
-.nearestPartial = function() {
+.nearestPartial = function(f0) {
+  var frequencyQuote = Math.pow(2, (this._tone - f0) / 12);
+  var partialBellow = Math.floor(frequencyQuote);
+  var partialAbove = Math.ceil(frequencyQuote);
+  var quoteDown = frequencyQuote / partialBellow;
+  var quoteUp = partialAbove / frequencyQuote;
+  if(quoteDown > Math.pow(2, 1 / 24) && quoteUp > Math.pow(2, 1 / 24)) {
+    return null;
+  }
   
+  return quoteDown < quoteUp ? partialBellow : partialAbove;
 };
 
 Note.prototype
