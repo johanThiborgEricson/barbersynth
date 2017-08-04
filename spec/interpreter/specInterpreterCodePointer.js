d354917fa0b6c0dd57b2f55baefcafa3a3d47e7d
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-08-04 17:16:55 +0200

    I removed the specification files for the interpreter method factory, since it is a project of its own now.

--- spec/interpreter/specInterpreterCodePointer.js
+++
@@ -1,8 +0,0 @@
-describe("InterpreterMethodFactory().CodePointer()", function() {
-  it("exists", function () {
-    var mf = InterpreterMethodFactory();
-    expect(mf.CodePointer).toBeDefined();
-  });
-  
-  // TODO: move CodePointer method tests here, except parse
-});
